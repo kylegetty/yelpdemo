6be8c7100e86c40e7c8b076b80915599bdf7a35f
Kyle Getty <uokylegetty2gmail.com>
2015-11-05 22:16:36 -0800

    carrierwave and imagemagik

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151106042748) do
+ActiveRecord::Schema.define(version: 20151106054326) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20151106042748) do
     t.string   "website"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
 end
